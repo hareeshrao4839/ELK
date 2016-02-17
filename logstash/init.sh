30753485f99f6c54eb2fa8c2e381efa338e94d55
Bart <contact@bartbania.com>
2016-02-17 08:07:36 +0000

    logst

--- logstash/init.sh
+++ logstash/init.sh
@@ -8,8 +8,10 @@ if [ "$1" = 'init' ]; then
   systemctl enable iptables
   chkconfig logstash on
   systemctl restart iptables
-  sed -i "s/Extensions for a typical CA/a subjectAltName = IP: LO_SERVER_IP" /etc/pki/tls/openssl.cnf
+  sed -i "/Extensions for a typical CA/a subjectAltName = IP: LO_SERVER_IP" /etc/pki/tls/openssl.cnf
   openssl req -config /etc/pki/tls/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:4096 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt
+  ssh-keygen -q -t rsa -b 4096 -N "" -f /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa*
   sshpass -p "EL_SERVER_PASS" ssh-copy-id -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no EL_SERVER_IP
   sshpass -p "KI_SERVER_PASS" ssh-copy-id -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no KI_SERVER_IP
   scp -i /root/.ssh/id_rsa.pub -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /etc/pki/tls/certs/logstash-forwarder.crt EL_SERVER_IP:/etc/pki/tls/certs/logstash-forwarder.crt
