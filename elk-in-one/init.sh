1c143f1f767196f2581b56b7be4c3a4f577751cc
Bart Bania <contact@bartbania.com>
2016-02-17 14:07:32 +0000

    elk-in-one

--- elk-in-one/init.sh
+++ elk-in-one/init.sh
@@ -6,13 +6,14 @@ if [ "$1" = 'init' ]; then
   #Start services
   systemctl daemon-reload
   systemctl enable iptables
+  systemctl enable nginx
   chkconfig logstash on
   systemctl enable elasticsearch
-  systemctl enable nginx
   chkconfig kibana on
+  systemctl enable filebeat packetbeat topbeat
 
   # Jelastic specific instructions
-  sed -i "/Extensions for a typical CA/a subjectAltName = IP: LO_SERVER_IP" /etc/pki/tls/openssl.cnf
+  sed -i "/Extensions for a typical CA/a subjectAltName = IP: EL_SERVER_IP" /etc/pki/tls/openssl.cnf
   openssl req -config /etc/pki/tls/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:4096 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt
 
   systemctl restart iptables
@@ -28,6 +29,8 @@ if [ "$1" = 'init' ]; then
   $CURL -XPUT "http://${EL_SERVER_IP}:9200/_template/filebeat?pretty" -d@/tmp/filebeat.template.json 
   $CURL -XPUT "http://${EL_SERVER_IP}:9200/_template/topbeat?pretty" -d@/tmp/topbeat.template.json 
   $CURL -XPUT "http://${EL_SERVER_IP}:9200/_template/packetbeat?pretty" -d@/tmp/packetbeat.template.json 
+
+  systemctl restart filebeat packetbeat topbeat
 fi
 
 exec "$@"
