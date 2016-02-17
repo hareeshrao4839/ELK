47cd0a64d7950cfe9bda02be16d858c41ba1b10b
Bart Bania <contact@bartbania.com>
2016-02-17 13:53:11 +0000

    elk-in-one

---
+++ elk-in-one/init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+if [ "$1" = 'init' ]; then
+  #Start services
+  systemctl daemon-reload
+  systemctl enable iptables
+  chkconfig logstash on
+  systemctl enable elasticsearch
+  systemctl enable nginx
+  chkconfig kibana on
+
+  # Jelastic specific instructions
+  sed -i "/Extensions for a typical CA/a subjectAltName = IP: LO_SERVER_IP" /etc/pki/tls/openssl.cnf
+  openssl req -config /etc/pki/tls/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:4096 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt
+
+  systemctl restart iptables
+  systemctl restart nginx
+  systemctl restart elasticsearch
+  sleep 20
+  /etc/init.d/logstash restart
+  sleep 20
+  /etc/init.d/kibana restart
+
+  #Add templates
+  cd /tmp; ./load.sh
+  $CURL -XPUT "http://${EL_SERVER_IP}:9200/_template/filebeat?pretty" -d@/tmp/filebeat.template.json 
+  $CURL -XPUT "http://${EL_SERVER_IP}:9200/_template/topbeat?pretty" -d@/tmp/topbeat.template.json 
+  $CURL -XPUT "http://${EL_SERVER_IP}:9200/_template/packetbeat?pretty" -d@/tmp/packetbeat.template.json 
+fi
+
+exec "$@"
+
