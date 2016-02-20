a75096723c5227d20614bb84d1e0df6fe4832363
Bart Bania <contact@bartbania.com>
2016-02-20 17:13:36 +0000

    cleaning

--- kibana/init.sh
+++ kibana/init.sh
@@ -11,7 +11,7 @@ if [ "$1" = 'init' ]; then
   $CURL -XPUT "http://${ELASTIC_IP}:9200/_template/filebeat?pretty" -d@/tmp/filebeat.template.json 
   $CURL -XPUT "http://${ELASTIC_IP}:9200/_template/topbeat?pretty" -d@/tmp/topbeat.template.json 
   $CURL -XPUT "http://${ELASTIC_IP}:9200/_template/packetbeat?pretty" -d@/tmp/packetbeat.template.json 
-  cd /tmp; ./load.sh
+  cd /tmp/beats-dashboards-1.1.0; ./load.sh
 fi
 
 exec "$@"
