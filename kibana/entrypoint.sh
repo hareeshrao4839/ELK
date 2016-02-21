6ee5754b83d3a09493361010cfc82452d5c92ce3
Bart Bania <contact@bartbania.com>
2016-02-21 18:06:14 +0000

    kibana supervised

--- kibana/entrypoint.sh
+++ kibana/entrypoint.sh
@@ -2,20 +2,9 @@
 
 set -e
 
-# Add kibana as command if needed
-if [[ "$1" == -* ]]; then
-  set -- kibana "$@"
-fi
+systemctl daemon-reload
+systemctl enable iptables
+systemctl restart iptables
 
-# Run as user "kibana" if the command is "kibana"
-if [ "$1" = 'kibana' ]; then
-  systemctl daemon-reload
-  systemctl enable iptables
-  systemctl restart iptables
-
-  nginx &
-  gosu kibana tini -- "$@"
-fi
-
-exec "$@"
+supervisord -c /etc/supervisord.conf
 
