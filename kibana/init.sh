91144327b3e43899235c481aa644be6871f43eed
Bart Bania <contact@bartbania.com>
2016-02-23 17:38:15 +0000

    rsync

--- kibana/init.sh
+++ kibana/init.sh
@@ -3,16 +3,11 @@
 set -e
 
 CURL=`which curl`
-
 ELASTIC_IP='ELK_SERVER_IP'
 
-if [ "$1" = 'init' ]; then
-  #Add templates
-  $CURL -XPUT "http://${ELASTIC_IP}:9200/_template/filebeat?pretty" -d@/tmp/filebeat.template.json 
-  $CURL -XPUT "http://${ELASTIC_IP}:9200/_template/topbeat?pretty" -d@/tmp/topbeat.template.json 
-  $CURL -XPUT "http://${ELASTIC_IP}:9200/_template/packetbeat?pretty" -d@/tmp/packetbeat.template.json 
-  cd /tmp/beats-dashboards-1.1.0; ./load.sh
-fi
-
-exec "$@"
+$CURL -XPUT "http://${ELASTIC_IP}:9200/_template/filebeat?pretty" -d@/tmp/filebeat.template.json 
+$CURL -XPUT "http://${ELASTIC_IP}:9200/_template/topbeat?pretty" -d@/tmp/topbeat.template.json 
+$CURL -XPUT "http://${ELASTIC_IP}:9200/_template/packetbeat?pretty" -d@/tmp/packetbeat.template.json 
+cd /tmp/beats-dashboards-1.1.0; ./load.sh; cd /
+rm -r /tmp/beats-dashboards-1.1.0
 
